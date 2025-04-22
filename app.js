b06d5bf3a71180f1d65a3a4c84201bcd4f34012e
lofispace <aarondjezierski@gmail.com>
2025-04-22 18:39:46 -0400

    created shuffle array function, and a set category titles function.

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const table = document.getElementById("jeopardy");
 let categoryIds = [];
 let gameDataList = [];
 
+// const orderedArray = [1,2,3,4,5,6,7,8];
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * i);
+        [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+    }
+    return array;
+}
+
+function setCategoryTitles(categoryData) {
+    const headers = document.getElementsByClassName("categoryTitle");
+    const titles = categoryData.map((category) => category.title);
+
+    for (let i = 0; i < titles.length; i++) {
+        console.log(headers[i].innerHTML = titles[i]);
+    }
+}
+
 async function fetchQuestionsAnswers(categoryId){
   try{
     const URL = `https://rithm-jeopardy.herokuapp.com/api/category?id=${categoryId}`;
@@ -33,6 +52,16 @@ async function fetchCategories() {
 startButton.addEventListener("click", async function(e){
     startButton.innerText="Restart?";
     gameDataList = [];
+
+    // for now i want the table to be blank
+    // 1. First I want to create the header ///
+    // 1a = Go into <thead> element create five <th> elements and insert category names 
+
+    // 2. After also programatically create 5 rows and 5 columns and insert questions marks
+    // 2a Also on each loop we need to put in the coordinates 
+    // 2b Render everything 
+
+
     for (const cell of gameCells) {
         cell.innerHTML = "?";
         cell.className = "gameCell"; 
@@ -44,20 +73,26 @@ startButton.addEventListener("click", async function(e){
     
     categoryIds = [...data];
 
-const rawData = await fetchQuestionsAnswers(categoryIds[0]);
+    // const rawData = await fetchQuestionsAnswers(categoryIds[0]);
 
-for (let i = 0; i < 5; i++ ){
-    const rawData = await fetchQuestionsAnswers(categoryIds[i]);
-    const gameData = {
-        title: rawData.title,
-        data: rawData.clues.map((clue) =>({ question: clue.question, answer: clue.answer }))
-    };
-    gameDataList.push(gameData);
+    // first I'm going to shuffle the category ids we got
+    shuffleArray(categoryIds);
+    // then I will pull questions and answers
+    for (let i = 0; i < 5; i++ ) {
+        const rawData = await fetchQuestionsAnswers(categoryIds[i]);
+        const gameData = {
+            title: rawData.title,
+            data: rawData.clues.map((clue) =>({ question: clue.question, answer: clue.answer }))
+        };
+        gameDataList.push(gameData);
 
-}
-jeopardy.style.display ="table";
+    }
 
-console.log(gameDataList);
+    setCategoryTitles(gameDataList);
+
+    jeopardy.style.display ="table";
+
+    // console.log(gameDataList);
 });
 
 for (const cell of gameCells){
